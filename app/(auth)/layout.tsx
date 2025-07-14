b43fc9260a6c3e79c2ae358a29ca00b3a904cdf1
Krish Koria <krishkoria2004@gmail.com>
2025-07-14 22:14:39 +0530

    Implement code changes to enhance functionality and improve performance

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,9 @@
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowLeftIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "@/public/logo.png";
+
 export default function AuthLayout({
   children,
 }: Readonly<{
@@ -5,7 +11,32 @@ export default function AuthLayout({
 }>) {
   return (
     <div className="relative flex min-h-svh flex-col items-center justify-center">
-      <div className="flex w-full max-w-sm flex-col gap-6 ">{children}</div>
+      <Link
+        href="/"
+        className={buttonVariants({
+          variant: "outline",
+          className: "absolute left-4 top-4",
+        })}
+      >
+        <ArrowLeftIcon className="size-4" />
+        Back
+      </Link>
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link
+          href="/"
+          className="flex items-center gap-2 self-center font-medium"
+        >
+          <Image
+            src={Logo}
+            alt="logo"
+            width={32}
+            height={32}
+            className="rounded-sm"
+          />
+          MindSpring
+        </Link>
+        {children}
+      </div>
     </div>
   );
 }
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -7,6 +7,8 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Form } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { GithubIcon } from "lucide-react";
 
 export default function LoginPage() {
@@ -18,7 +20,7 @@ export default function LoginPage() {
           Login with your Github or Email to continue.
         </CardDescription>
       </CardHeader>
-      <CardContent>
+      <CardContent className="flex flex-col gap-4">
         <Button className="w-full" variant="outline">
           <GithubIcon className="size-4" />
           Continue with Github
@@ -27,6 +29,13 @@ export default function LoginPage() {
           <span className="relative z-10 bg-card px-2 text-muted-foreground">
             OR CONTINUE WITH
           </span>
+        </div>
+        <div className="grid gap-3">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input type="email" id="email" placeholder="m@example.com" />
+          </div>
+          <Button>Continue with Email</Button>
         </div>
       </CardContent>
     </Card>
