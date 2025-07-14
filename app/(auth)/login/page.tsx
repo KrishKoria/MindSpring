9adfae84a75238d318a8d5abb0f33a8c201a1a7f
Krish Koria <krishkoria2004@gmail.com>
2025-07-14 21:48:59 +0530

    Implement login page with GitHub login option and theme provider integration

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,18 +1,34 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Form } from "@/components/ui/form";
+import { GithubIcon } from "lucide-react";
+
 export default function LoginPage() {
   return (
-    <div>
-      <h1>Login</h1>
-      <form>
-        <label>
-          Email:
-          <input type="email" name="email" />
-        </label>
-        <label>
-          Password:
-          <input type="password" name="password" />
-        </label>
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-xl">Welcome Back!</CardTitle>
+        <CardDescription>
+          Login with your Github or Email to continue.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Button className="w-full" variant="outline">
+          <GithubIcon className="size-4" />
+          Continue with Github
+        </Button>
+        <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+          <span className="relative z-10 bg-card px-2 text-muted-foreground">
+            OR CONTINUE WITH
+          </span>
+        </div>
+      </CardContent>
+    </Card>
   );
 }
