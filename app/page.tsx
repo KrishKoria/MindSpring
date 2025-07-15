a87e0318ce486d9783699b9109c7a42d8731ff6d
Krish Koria <krishkoria2004@gmail.com>
2025-07-15 12:31:25 +0530

    feat: refactor Home component to use authClient for session management and logout functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
+"use client";
 import { ThemeToggle } from "@/components/theme-toggle";
-import { auth } from "@/lib/auth";
-import { headers } from "next/headers";
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
-export default async function Home() {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
+export default function Home() {
+  const { data: session } = authClient.useSession();
+  const router = useRouter();
+  async function signOut() {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          toast.success("Successfully logged out");
+          router.push("/");
+        },
+      },
+    });
+  }
   return (
     <div className="p-24">
       <ThemeToggle />
@@ -13,11 +25,13 @@ export default async function Home() {
         <div>
           <h1>Welcome back, {session.user.name}!</h1>
           <p>Your email is: {session.user.email}</p>
+          <Button onClick={() => authClient.signOut()}>Logout</Button>
         </div>
       ) : (
         <div>
           <h1>Welcome to our app!</h1>
           <p>Please log in to continue.</p>
+          <Button onClick={() => router.push("/login")}>Login</Button>
         </div>
       )}
     </div>
