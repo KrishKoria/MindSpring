a9baf8aacd969b042dcf3a741ad6026ee70de9e8
Krish Koria <krishkoria2004@gmail.com>
2025-07-15 19:07:49 +0530

    feat: integrate Arcjet for bot detection and signup protection

---
+++ lib/arcjet.ts
@@ -0,0 +1,30 @@
+import arcjet, {
+  detectBot,
+  fixedWindow,
+  protectSignup,
+  sensitiveInfo,
+  shield,
+  slidingWindow,
+} from "@arcjet/next";
+import { env } from "./env";
+
+export {
+  detectBot,
+  fixedWindow,
+  protectSignup,
+  sensitiveInfo,
+  shield,
+  slidingWindow,
+};
+
+const aj = arcjet({
+  key: env.ARCJET_API_KEY,
+  characteristics: ["fingerprint"],
+  rules: [
+    shield({
+      mode: "LIVE",
+    }),
+  ],
+});
+
+export default aj;
