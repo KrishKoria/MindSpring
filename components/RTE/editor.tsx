c1a97ebcf7cdce39a19d46b7372aea53b8cebf85
Krish Koria <krishkoria2004@gmail.com>
2025-07-22 20:44:11 +0530

    chore: update dependencies and fix versioning issues
    - Bump @tabler/icons-react from 3.34.0 to 3.34.1
    - Update better-auth from 1.2.12 to 1.3.2
    - Upgrade @types/node from 24.0.15 to 24.1.0
    - Add @tiptap/extension-text-align with version 3.0.7
    - Update eslint-related packages to version 8.38.0
    - Downgrade jose from 6.0.12 to 5.10.0
    - Update zod from 3.25.76 to 4.0.5

--- components/RTE/editor.tsx
+++ components/RTE/editor.tsx
@@ -1,16 +1,27 @@
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Menubar from "./menubar";
-
+import TextAlign from "@tiptap/extension-text-align";
 const RTEditor = () => {
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit,
+      TextAlign.configure({
+        types: ["heading", "paragraph"],
+      }),
+    ],
     immediatelyRender: false,
+    editorProps: {
+      attributes: {
+        class: "min-h-[300px] p-4 focus:outline-none",
+      },
+    },
   });
 
   return (
-    <div>
+    <div className="w-full border border-input rounded-lg overflow-hidden dark:bg-input/30">
       <Menubar editor={editor} />
+      <EditorContent editor={editor} />
     </div>
   );
 };
--- components/RTE/menubar.tsx
+++ components/RTE/menubar.tsx
@@ -7,17 +7,22 @@ import {
 } from "../ui/tooltip";
 import { Toggle } from "../ui/toggle";
 import {
+  AlignCenter,
+  AlignLeft,
+  AlignRight,
   Bold,
   Heading1,
   Heading2,
   Heading3,
-  HistoryIcon,
   Italic,
   List,
   ListOrdered,
+  Redo,
   Strikethrough,
+  Undo,
 } from "lucide-react";
 import { cn } from "@/lib/utils";
+import { Button } from "../ui/button";
 
 interface MenubarProps {
   editor: Editor | null;
@@ -29,14 +34,16 @@ export default function Menubar({ editor }: MenubarProps) {
   }
 
   return (
-    <div className="border border-input rounded-t-lg p-2 bg-card flex flex-wrap gap-1 items-center">
+    <div className="border border-input border-t-0 border-x-0 rounded-t-lg p-2 bg-card flex flex-wrap gap-1 items-center">
       <TooltipProvider>
         <div className="flex flex-wrap gap-1">
           <Tooltip>
             <TooltipTrigger asChild>
               <span>
                 <Toggle
-                  onPressedChange={() => editor.chain().toggleBold().run()}
+                  onPressedChange={() =>
+                    editor.chain().focus().toggleBold().run()
+                  }
                   className={cn(
                     editor.isActive("bold") && "bg-muted text-muted-foreground"
                   )}
@@ -52,7 +59,9 @@ export default function Menubar({ editor }: MenubarProps) {
             <TooltipTrigger asChild>
               <span>
                 <Toggle
-                  onPressedChange={() => editor.chain().toggleItalic().run()}
+                  onPressedChange={() =>
+                    editor.chain().focus().toggleItalic().run()
+                  }
                   className={cn(
                     editor.isActive("italic") &&
                       "bg-muted text-muted-foreground"
@@ -69,7 +78,9 @@ export default function Menubar({ editor }: MenubarProps) {
             <TooltipTrigger asChild>
               <span>
                 <Toggle
-                  onPressedChange={() => editor.chain().toggleStrike().run()}
+                  onPressedChange={() =>
+                    editor.chain().focus().toggleStrike().run()
+                  }
                   className={cn(
                     editor.isActive("strike") &&
                       "bg-muted text-muted-foreground"
@@ -87,7 +98,7 @@ export default function Menubar({ editor }: MenubarProps) {
               <span>
                 <Toggle
                   onPressedChange={() =>
-                    editor.chain().toggleHeading({ level: 1 }).run()
+                    editor.chain().focus().toggleHeading({ level: 1 }).run()
                   }
                   className={cn(
                     editor.isActive("heading", { level: 1 }) &&
@@ -106,7 +117,7 @@ export default function Menubar({ editor }: MenubarProps) {
               <span>
                 <Toggle
                   onPressedChange={() =>
-                    editor.chain().toggleHeading({ level: 2 }).run()
+                    editor.chain().focus().toggleHeading({ level: 2 }).run()
                   }
                   className={cn(
                     editor.isActive("heading", { level: 2 }) &&
@@ -125,7 +136,7 @@ export default function Menubar({ editor }: MenubarProps) {
               <span>
                 <Toggle
                   onPressedChange={() =>
-                    editor.chain().toggleHeading({ level: 3 }).run()
+                    editor.chain().focus().toggleHeading({ level: 3 }).run()
                   }
                   className={cn(
                     editor.isActive("heading", { level: 3 }) &&
@@ -144,7 +155,7 @@ export default function Menubar({ editor }: MenubarProps) {
               <span>
                 <Toggle
                   onPressedChange={() =>
-                    editor.chain().toggleBulletList().run()
+                    editor.chain().focus().toggleBulletList().run()
                   }
                   className={cn(
                     editor.isActive("bulletList") &&
@@ -163,7 +174,7 @@ export default function Menubar({ editor }: MenubarProps) {
               <span>
                 <Toggle
                   onPressedChange={() =>
-                    editor.chain().toggleOrderedList().run()
+                    editor.chain().focus().toggleOrderedList().run()
                   }
                   className={cn(
                     editor.isActive("orderedList") &&
@@ -179,6 +190,105 @@ export default function Menubar({ editor }: MenubarProps) {
           </Tooltip>
         </div>
         <div className="w-px h-6 bg-border mx-2"></div>
+        <div className="flex flex-wrap gap-1">
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <span>
+                <Toggle
+                  onPressedChange={() =>
+                    editor.chain().focus().setTextAlign("left").run()
+                  }
+                  className={cn(
+                    editor.isActive({
+                      textAlign: "left",
+                    }) && "bg-muted text-muted-foreground"
+                  )}
+                >
+                  <AlignLeft />
+                </Toggle>
+                <span></span>
+              </span>
+            </TooltipTrigger>
+            <TooltipContent>Left Align Text</TooltipContent>
+          </Tooltip>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <span>
+                <Toggle
+                  onPressedChange={() =>
+                    editor.chain().focus().setTextAlign("center").run()
+                  }
+                  className={cn(
+                    editor.isActive({
+                      textAlign: "center",
+                    }) && "bg-muted text-muted-foreground"
+                  )}
+                >
+                  <AlignCenter />
+                </Toggle>
+                <span></span>
+              </span>
+            </TooltipTrigger>
+            <TooltipContent>Center Align Text</TooltipContent>
+          </Tooltip>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <span>
+                <Toggle
+                  onPressedChange={() =>
+                    editor.chain().focus().setTextAlign("right").run()
+                  }
+                  className={cn(
+                    editor.isActive({
+                      textAlign: "right",
+                    }) && "bg-muted text-muted-foreground"
+                  )}
+                >
+                  <AlignRight />
+                </Toggle>
+                <span></span>
+              </span>
+            </TooltipTrigger>
+            <TooltipContent>Right Align Text</TooltipContent>
+          </Tooltip>
+        </div>
+        <div className="w-px h-6 bg-border mx-2"></div>
+        <div className="flex flex-wrap gap-1">
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <span>
+                <Button
+                  size={"sm"}
+                  variant={"ghost"}
+                  type="button"
+                  onClick={() => editor.chain().undo().run()}
+                  disabled={!editor.can().undo()}
+                >
+                  <Undo />
+                </Button>
+                <span></span>
+              </span>
+            </TooltipTrigger>
+            <TooltipContent>Undo</TooltipContent>
+          </Tooltip>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <span>
+                <Button
+                  size={"sm"}
+                  variant={"ghost"}
+                  type="button"
+                  onClick={() => editor.chain().redo().run()}
+                  disabled={!editor.can().redo()}
+                >
+                  <Redo />
+                </Button>
+                <span></span>
+              </span>
+            </TooltipTrigger>
+            <TooltipContent>Redo</TooltipContent>
+          </Tooltip>
+        </div>
       </TooltipProvider>
     </div>
   );
