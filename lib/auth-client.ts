f426175c74f0edc8a9208d26afd8cd45979dfa43
Krish Koria <krishkoria2004@gmail.com>
2025-07-25 21:42:39 +0530

    feat(auth): update baseURL to use environment variable for better configuration

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,8 @@
 import { createAuthClient } from "better-auth/react";
 import { emailOTPClient, adminClient } from "better-auth/client/plugins";
+import { env } from "./env";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL: env.BETTER_AUTH_URL,
   plugins: [emailOTPClient(), adminClient()],
 });
