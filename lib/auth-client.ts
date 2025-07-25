25f9a2848c1d66d3cd1bac1dcc0eb831833dc765
Krish Koria <krishkoria2004@gmail.com>
2025-07-25 21:48:58 +0530

    fix(auth): remove baseURL configuration from authClient

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,8 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 import { emailOTPClient, adminClient } from "better-auth/client/plugins";
-import { env } from "./env";
 
 export const authClient = createAuthClient({
-  baseURL: env.BETTER_AUTH_URL,
   plugins: [emailOTPClient(), adminClient()],
 });
