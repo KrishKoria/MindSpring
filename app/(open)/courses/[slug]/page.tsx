c14773d7fe2bf3c82035cf225810405f52a755fd
Krish Koria <krishkoria2004@gmail.com>
2025-07-27 20:03:39 +0530

    feat(courses): enhance CoursePage with pricing details and course features

--- app/(open)/courses/[slug]/page.tsx
+++ app/(open)/courses/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import RenderRT from "@/components/RTE/render";
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import {
   Collapsible,
@@ -10,12 +11,14 @@ import { Separator } from "@/components/ui/separator";
 import useConstructUrl from "@/hooks/use-construct-url";
 import getCourse from "@/lib/data/public/get-course";
 import {
+  IconBook,
   IconCategory,
   IconChartBar,
   IconChevronDown,
   IconClock,
   IconPlayerPlay,
 } from "@tabler/icons-react";
+import { CheckIcon } from "lucide-react";
 import Image from "next/image";
 
 type Params = Promise<{ slug: string }>;
@@ -140,6 +143,143 @@ export default async function CoursePage({ params }: { params: Params }) {
               </Collapsible>
             ))}
           </div>
+        </div>
+      </div>
+      <div className="order-2 lg:col-span-1">
+        <div className="sticky top-20">
+          <Card className="py-0">
+            <CardContent className="p-6">
+              <div className="flex justify-between items-center mb-6">
+                <span className="text-lg font-medium">Price:</span>
+                <span className="text-2xl font-bold text-primary">
+                  {new Intl.NumberFormat("en-IN", {
+                    style: "currency",
+                    currency: "INR",
+                  }).format(course.price)}
+                </span>
+              </div>
+              <div className="mb-6 space-y-3 rounded-lg  bg-primary/10 p-4">
+                <h4 className="font-medium">What you'll get</h4>
+                <div className="flex flex-col gap-3">
+                  <div className="flex items-center gap-3">
+                    <div className="flex size-8 items-center justify-center rounded-full bg-primary/10 text-primary">
+                      <IconClock className="size-4" />
+                    </div>
+                    <div>
+                      <p className="text-sm text-muted-foreground">
+                        <span className="font-semibold text-primary">
+                          {course.duration} hours
+                        </span>{" "}
+                        of learning content
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex items-center gap-3">
+                    <div className="flex size-8 items-center justify-center rounded-full bg-primary/10 text-primary">
+                      <IconChartBar className="size-4" />
+                    </div>
+                    <div>
+                      <p className="text-sm text-muted-foreground">
+                        <span className="font-semibold text-primary">
+                          {course.level.charAt(0).toUpperCase() +
+                            course.level.slice(1).toLowerCase()}
+                        </span>{" "}
+                        mastery: Perfect for {course.level.toLowerCase()}{" "}
+                        learners ready to level up!
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex items-center gap-3">
+                    <div className="flex size-8 items-center justify-center rounded-full bg-primary/10 text-primary">
+                      <IconCategory className="size-4" />
+                    </div>
+                    <div>
+                      <p className="text-sm text-muted-foreground">
+                        Dive deep into the world of{" "}
+                        {
+                          <span className="font-semibold text-primary">
+                            {course.category.charAt(0).toUpperCase() +
+                              course.category.slice(1).toLowerCase()}
+                          </span>
+                        }
+                        —explore, learn, and master!
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex items-center gap-3">
+                    <div className="flex size-8 items-center justify-center rounded-full bg-primary/10 text-primary">
+                      <IconBook className="size-4" />
+                    </div>
+                    <div>
+                      <p className="text-sm text-muted-foreground">
+                        <span className="font-semibold text-primary">
+                          {(() => {
+                            const lessonCount = course.chapters.reduce(
+                              (acc, chapter) => acc + chapter.lessons.length,
+                              0
+                            );
+                            return lessonCount === 1
+                              ? "1 engaging"
+                              : `${lessonCount} action-packed`;
+                          })()}
+                        </span>
+                        {` lesson${
+                          course.chapters.reduce(
+                            (acc, chapter) => acc + chapter.lessons.length,
+                            0
+                          ) !== 1
+                            ? "s"
+                            : ""
+                        }`}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="mb-6 space-y-3">
+                <h4>
+                  This course is designed to be accessible and engaging for all
+                  learners, regardless of their background or experience level.
+                </h4>
+                <Separator className="text-accent-foreground border-1" />
+                <h4>This Course Includes:</h4>
+                <ul className="space-y-2">
+                  <li className="flex items-center gap-2 text-sm">
+                    <div className="rounded-full bg-primary/10 p-1 text-primary">
+                      <CheckIcon className="size-3" />
+                    </div>
+                    <span>Engaging video lessons</span>
+                  </li>
+                  <li className="flex items-center gap-2 text-sm">
+                    <div className="rounded-full bg-primary/10 p-1 text-primary">
+                      <CheckIcon className="size-3" />
+                    </div>
+                    <span>Interactive quizzes and assignments</span>
+                  </li>
+                  <li className="flex items-center gap-2 text-sm">
+                    <div className="rounded-full bg-primary/10 p-1 text-primary">
+                      <CheckIcon className="size-3" />
+                    </div>
+                    <span>Hands-on projects and case studies</span>
+                  </li>
+                  <li className="flex items-center gap-2 text-sm">
+                    <div className="rounded-full bg-primary/10 p-1 text-primary">
+                      <CheckIcon className="size-3" />
+                    </div>
+                    <span>Certificate of completion</span>
+                  </li>
+                </ul>
+              </div>
+              <Button variant="outline" className="w-full">
+                Enroll Now!
+              </Button>
+              <p className="mt-3 text-center text-xs text-muted-foreground">
+                Join our community of learners and start your journey today!{" "}
+                <br />
+                Cancel anytime, no questions asked.
+              </p>
+            </CardContent>
+          </Card>
         </div>
       </div>
     </div>
